44e19372289503464be3d2c6669ff0345d4ce353
amir <amirtaki623@gmail.com>
2025-09-06 22:34:22 +0330

    part 15

--- src/main.js
+++ src/main.js
@@ -2,3 +2,8 @@ const colorOne = document.getElementById("color-a")
 const colorTwo = document.getElementById("color-b")
 const currentDirection = 'to bottom';
 const outputCode = document.getElementById('code') 
+
+
+const setDirection = (value,_this) =>{
+    let directions = document.querySelectorAll(".buttons button")
+}
