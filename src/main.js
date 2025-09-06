b90399ab44f0f61afd25e0ef546c441a652b0ded
amir <amirtaki623@gmail.com>
2025-09-06 22:33:46 +0330

    part 14

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
 const colorOne = document.getElementById("color-a")
 const colorTwo = document.getElementById("color-b")
+const currentDirection = 'to bottom';
+const outputCode = document.getElementById('code') 
